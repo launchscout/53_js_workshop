516fcf8813756f373024c3f70a7eb653c15824a1
Chris Nelson <chris@gaslightsoftware.com>
2013-12-01 22:40:32 -0500

    Lab 2 passes

--- labs/lab2.js
+++ labs/lab2.js
@@ -1,6 +1,7 @@
 describe("Let's make an object", function() {
   var thing = {
-
+    name: "Thing 1",
+    description: "A good thing."
   };
   it("has the right properties", function() {
     expect(thing.name).toEqual("Thing 1");
