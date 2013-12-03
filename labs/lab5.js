f97fed6a608344bd9bc1eb911b37984418e78c7c
Chris Nelson <chris@gaslightsoftware.com>
2013-12-02 21:46:06 -0500

    Lab 5 start point

---
+++ labs/lab5.js
@@ -0,0 +1,21 @@
+describe("equals and threequals", function() {
+    var truthTable = [
+        5 == 5,
+        5 == 6,
+        5 === 5,
+        5 == "5",
+        5 === "5",
+        null == undefined,
+        null === undefined,
+        0 == '',
+        '' == 0,
+        0 === '',
+        '' == 0,
+    ];
+
+    it("should make a big difference which you choose", function() {
+        expect(truthTable[0]).toBeTruthy();
+        expect(truthTable[1]).toBeFalsy();
+        // you fill in the rest!
+    });
+});
