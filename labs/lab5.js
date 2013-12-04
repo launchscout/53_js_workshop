0ae0c66e35232af59856e59840a92275b13c1293
Joel Turnbull <joelbywan@gmail.com>
2013-12-04 14:21:27 -0500

    finish lab 5

--- labs/lab5.js
+++ labs/lab5.js
@@ -16,6 +16,14 @@ describe("equals and threequals", function() {
     it("should make a big difference which you choose", function() {
         expect(truthTable[0]).toBeTruthy();
         expect(truthTable[1]).toBeFalsy();
-        // you fill in the rest!
+        expect(truthTable[2]).toBeTruthy();
+        expect(truthTable[3]).toBeTruthy();
+        expect(truthTable[4]).toBeFalsy();
+        expect(truthTable[5]).toBeTruthy();
+        expect(truthTable[6]).toBeFalsy();
+        expect(truthTable[7]).toBeTruthy();
+        expect(truthTable[8]).toBeTruthy();
+        expect(truthTable[9]).toBeFalsy();
+        expect(truthTable[10]).toBeFalsy();
     });
 });
