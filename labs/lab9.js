6df69c7f02f8b78b3197cf34fa14f8c78ce55fa8
Joel Turnbull <joelbywan@gmail.com>
2013-12-08 17:24:08 -0500

    lab 9

--- labs/lab9.js
+++ labs/lab9.js
@@ -5,37 +5,37 @@ describe("The Prototype Chain", function () {
       var nativeObjectProtoype = Object.prototype;
 
       it("link to Javascript's native Object.prototype", function () {
-        expect(mammal.__proto__).toEqual(Object.prototype);
+        expect(mammal.__proto__).toEqual(/* fill in */);
       });
       it("inherit properties and methods from the Object prototype", function () {
-        expect(mammal.toString()).toEqual(Object.prototype.toString());
+        expect(mammal.toString()).toEqual(/* fill in */);
       });
     });
     describe("objects created using the 'new' constructor", function () {
       var Mammal = function Mammal (species) {
         this.species = species;
         this.sayHello = function () {
-          return "Hi I'm a " + this.species;
+          // implement a function that will make line 33 and line 42 pass
         };
       };
       var human = new Mammal("human");
 
       it("link to the new's target function's prototype", function () {
-        expect(human.__proto__).toEqual(Mammal.prototype);
+        expect(human.__proto__).toEqual();
       });
       
       describe("inherit from the target's prototype", function (){ 
-        Mammal.prototype.furry = true
+        Mammal.prototype.furry = true;
         var bear = new Mammal("bear");
         
         it("properties and methods", function () {
-          expect(bear.furry).toEqual(true);
+          expect(bear.furry).toEqual(/* fill in */);
           expect(bear.sayHello()).toEqual("Hi I'm a bear");
         });
 
         describe("that can be overridden", function () {
           var dolphin = new Mammal("dolphin");
-          dolphin.furry = false
+          // override the value of furry on the dolphin object to make line 39 pass
 
           it("on the object", function () {
             expect(dolphin.furry).toEqual(false);
@@ -43,7 +43,7 @@ describe("The Prototype Chain", function () {
           });
 
           it("but don't affect properties or methods of the target's prototype", function () {
-            expect(Mammal.prototype.furry).toEqual(true);
+            expect(Mammal.prototype.furry).toEqual(/* fill in */);
           });
         });
       });
