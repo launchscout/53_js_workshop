3f56f3b3545ca248a85e7e5ab2b0bd368e9382a2
Chris Nelson <chris@gaslightsoftware.com>
2013-12-08 22:06:13 -0500

    passing spec

--- labs/shopping_cart.js
+++ labs/shopping_cart.js
@@ -1,2 +1,17 @@
 var Shopping = {};
-Shopping.Cart = function() {};
+Shopping.Cart = function() {
+    this.items = [];
+};
+
+Shopping.Cart.prototype = {
+    addItem: function(item) {
+        this.items.push(item);
+    },
+    total: function() {
+        var itemTotal = 0;
+        for(var i=0; i < this.items.length; i++) {
+            itemTotal += this.items[i].price;
+        }
+        return itemTotal;
+    }
+}
