3f813f42215377b9251cd569da6fcf38392c19a9
Chris Nelson <chris@gaslightsoftware.com>
2013-12-09 18:32:38 -0500

    finished ajax lab

--- labs/application.js
+++ labs/application.js
@@ -1,14 +1,22 @@
 // Do something awesome here!
 // hint: parseInt() converts a string to a number
 var cart = new Shopping.Cart();
+var allItems;
 $(function() {
     $("button").click(function(event) {
-        // get item somehow
+        item = allItems[$("#item_select").val()]
         cart.addItem(item);
         $("#cart").append($("<li>" + item.name + "</li>"));
         $("#item").val("");
         $("#price").val("");
         event.preventDefault();
         $("#total").text(cart.total());
+    });
+
+    $.getJSON("items.json", function(items) {
+        allItems = items;
+        items.forEach(function(item, index) {
+            $("#item_select").append($("<option value='" + index + "'>" + item.name + "</option>"));
+        });
     });
 });
