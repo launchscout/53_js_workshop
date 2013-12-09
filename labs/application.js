5dae14949704f4906350d866fad4ab663b0ac960
Chris Nelson <chris@gaslightsoftware.com>
2013-12-09 17:43:57 -0500

    works now

--- labs/application.js
+++ labs/application.js
@@ -1,2 +1,17 @@
 // Do something awesome here!
 // hint: parseInt() converts a string to a number
+var cart = new Shopping.Cart();
+$(function() {
+    $("button").click(function(event) {
+        var item = {
+            name: $("#item").val(),
+            price: parseInt($("#price").val())
+        };
+        cart.addItem(item);
+        $("#cart").append($("<li>" + item.name + "</li>"));
+        $("#item").val("");
+        $("#price").val("");
+        event.preventDefault();
+        $("#total").text(cart.total());
+    });
+});
